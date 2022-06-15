6ef322731fefd093100d8e0ee6dea49e5ad409b6
jholmes847 <93491719+jholmes847@users.noreply.github.com>
2022-06-15 08:59:02 -0700

    commit on 'jeff'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Add from './components/Add'
+import Edit from './components/Edit'
 
-function App() {
+
+
+const App = () => {
+
+  const [post, setPost] = useState([])
+
+
+  const getPost = () => {
+    axios.get('http://localhost:8000/api/post')
+    .then(response => setPost(response.data),
+    err=> console.log(err)
+  )
+  .catch(error=> console.error(error))
+  }
+  
+  const handleCreate = (setPost) => {
+    axios
+      .post('http://localhost:8000/api/post', setPost)
+      .then((response) => {
+        setPost([...post, response.data])
+      })
+  }
+
+ 
+
+
+  useEffect(() => {
+    getPost()
+   }, [])
+
   return (
-   <h1>Hello World</h1>
-  );
+    <>
+      <h1>App</h1>
+      <Add handleCreate={handleCreate} />
+      <div className="posts">
+ {post.map((person) => {
+   return (
+     <div className="post" key={post.id}>
+       <h4>Name: {post.name}</h4>
+       <h5>CPU: {post.cpu}</h5>
+     </div>
+   )
+ })}
+</div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Add.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+
+const Add = (props) => {
+    let emptyPost = {name: '', cpu: '', gpu: '', mobo: '', ram: '', psu: '', cooler:'', storage:'', case:''}
+    const [post, setPost] = useState(emptyPost)
+
+    const handleChange = (event) => {
+        setPost({...post, [event.target.name]: event.target.value})
+      }
+    
+      const handleSubmit = (event) => {
+        event.preventDefault()
+        props.handleCreate(post)
+        setPost({name: '', cpu: '', gpu: '', mobo: '', ram: '', psu: '', cooler:'', storage:'', case:''})
+      }
+    
+      return (
+        <>
+        <h3> Add Post</h3>
+        <form onSubmit={handleSubmit}>
+    <label htmlFor="name">Name: </label>
+    <input type="text" name="name" value={post.name} onChange=      {handleChange} />
+    <br />
+    <br />
+    <label htmlFor="cpu">CPU: </label>
+    <input type="text" name="cpu" value={post.cpu} onChange={handleChange} />
+    <br/>
+    <br/>
+    <label htmlFor="gpu">GPU: </label>
+    <input type="text" name="gpu" value={post.gpu} onChange={handleChange} />
+    <br />
+    <br />
+    <label htmlFor="mobo">Mobo: </label>
+    <input type="text" name="mobo" value={post.mobo} onChange={handleChange} />
+    <br />
+    <br />
+    <label htmlFor="ram">Ram: </label>
+    <input type="text" name="ram" value={post.ram} onChange={handleChange} />
+    <br />
+    <br />
+    <label htmlFor="psu">PSU: </label>
+    <input type="text" name="psu" value={post.psu} onChange={handleChange} />
+    <br />
+    <br />
+    <label htmlFor="cooler">Cooler: </label>
+    <input type="text" name="cooler" value={post.cooler} onChange={handleChange} />
+    <br />
+    <br />
+    <label htmlFor="storage">Storage: </label>
+    <input type="text" name="storage" value={post.storage} onChange={handleChange} />
+    <br />
+    <br />
+    <label htmlFor="case">Case: </label>
+    <input type="text" name="case" value={post.case} onChange={handleChange} />
+    <br/>
+    
+    <input type="submit" />
+</form>
+        
+        
+        </>
+
+      )
+}
+export default Add
