2c6f701aa62ebbf85b13c96e29daf0d34dcb7b4a
jholmes847 <93491719+jholmes847@users.noreply.github.com>
2022-06-16 16:17:12 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,29 +4,29 @@ import Add from './components/Add'
 import Edit from './components/Edit'
 
 
-
 const App = () => {
 
   const [post, setPost] = useState([])
 
-
   const getPost = () => {
-    axios.get('https://pc-backend-app.herokuapp.com/api/posts')
-    .then(response => setPost(response.data),
-    err=> console.log(err)
-  )
+    axios
+      .get('https://pc-backend-app.herokuapp.com/api/posts')
+      .then(response => setPost(response.data),
+      (err) => console.error(err)
+      )
+      .catch((error) => console.error(error))
   }
-  
+
   const handleCreate = (addPost) => {
     axios
       .post('https://pc-backend-app.herokuapp.com/api/posts', addPost)
       .then((response) => {
-        setPost([...post, response.data])
+        getPost()
       })
   }
 
-  const handleEdit = (editPost) => {
-    axios.put('https://pc-backend-app.herokuapp.com/api/posts' + editPost.id, editPost)
+  const handleUpdate = (editPost) => {
+axios.put('https://pc-backend-app.herokuapp.com/api/posts/' + editPost.id, editPost)
     .then((response) => {
       setPost(post.map((post) => {
         return post.id !== response.data.id ? post : response.data
@@ -35,55 +35,45 @@ const App = () => {
     })
   }
 
-  const handleDelete = (deletePost) => {
-    axios.delete('https://pc-backend-app.herokuapp.com/api/posts' + deletePost.id)
+  const handleDelete = (deletedPost) => {
+    axios.delete('https://pc-backend-app.herokuapp.com/api/posts/' + deletedPost.id)
     .then((response) => {
-      setPost(post.filter(post => post.id !== deletePost.id))
+      setPost(
+        post.filter(posts => posts.id !== deletedPost.id)
+      );
      
     })
   }
-
- 
-
-
   useEffect(() => {
     getPost()
-   }, [])
+  }, [])
 
   return (
     <>
       <h1>App</h1>
       <Add handleCreate={handleCreate} />
       <div className="posts">
- {post.map((person) => {
-   return (
-     <div className="post">
-      Name: {post.name}
-      <br></br>
-      Post: {post.post}
-      <br></br>
-      CPU: {post.cpu}
-      <br></br>
-      GPU: {post.gpu}
-      <br></br>
-      Mobo: {post.gpu}
-      <br></br>
-      Ram: {post.ram }
-      <br></br>
-      Psu: {post.cooler}
-      <br></br>
-      Storage: {post.storage}
-      <br></br>
-      Case: {post.storage}
-      <Edit handleUpdate={handleEdit} post={post}/>
-      <button onClick={() => {handleDelete(post)}}>
-            delete
-            </button>
-
-     </div>
-   )
- })}
-</div>
+        {post.map((post) => {
+          return (
+            <div className="post" key={post.id}>
+              <p>Name: {post.name}</p>
+              <p>Post: {post.post}</p>
+              <p>CPU: {post.cpu}</p>
+              <p>Cooler: {post.cooler}</p>
+              <p>MOBO: {post.mobo}</p>
+              <p>RAM: {post.ram}</p>
+              <p>PSU: {post.psu}</p>
+              <p>GPU: {post.gpu}</p>
+              <p>Storage: {post.storage}</p>
+              <p>Case: {post.case}</p>
+              <Edit handleUpdate={handleUpdate} post={post}/>
+              <button onClick={() => {handleDelete(post)}}>
+                Delete
+              </button>
+            </div>
+          )
+       })}
+      </div>
     </>
   )
 }
